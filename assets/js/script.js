a167fbed6fc759c22e867f73f1d49a2ad03974ec
Sávio Sérgio <168147001+SavioSrg@users.noreply.github.com>
2025-01-26 11:26:59 -0300

    Correção no menu responsivo
    Após clicar em um link do menu ele ira desaparecer novamente.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,11 @@
 const menu = document.getElementById('lista-menu');
 
 function mudouTamanho() {
-        if (window.innerWidth < 643) {
-            menu.style.display = 'none'
-        } else {
-            menu.style.display = 'flex'
-        }
+    if (window.innerWidth < 643) {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'flex'
+    }
 }
 
 function clickMenu() {
@@ -15,3 +15,22 @@ function clickMenu() {
         menu.style.display = 'none';
     }
 }
+
+
+const links = document.querySelectorAll('#lista-menu a');
+// Função para recolher o menu ao clicar em um link
+function linkClick() {
+    if (window.innerWidth < 643) {
+        // Só recolhe o menu se for em uma tela menor
+        menu.style.display = 'none';
+    }
+}
+
+// Adiciona o evento de clique em cada link do menu
+links.forEach(link => {
+    link.addEventListener('click', linkClick);
+});
+
+// Garante que o menu responda ao redimensionamento da tela
+window.addEventListener('resize', mudouTamanho);
+
